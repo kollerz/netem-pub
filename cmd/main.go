d71c549787143b63877025acb16d17713157e878
Fossati Thomas <thomas.fossati@nokia.com>
2018-02-20 10:24:21 +0000

    towards an executable

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"expvar"
+	"net/http"
+	"time"
+
+	"github.com/thomas-fossati/netem-pub/netem"
+)
+
+var pktCount = expvar.NewInt("packet.count")
+var pktDropped = expvar.NewInt("packet.dropped")
+var pktReordered = expvar.NewInt("packet.reordered")
+var bytesCount = expvar.NewInt("bytes.count")
+
+func NetemPoll() {
+	ifaces := []string{"eth0"} // TODO(tho) cfg
+	for {
+		for _, iface := range ifaces {
+			out, err := netem.Fetch(iface)
+			if err != nil {
+				continue
+			}
+
+			netemData, err := netem.Parse(out)
+			if err != nil {
+				continue
+			}
+
+			updateExpVars(netemData)
+
+		}
+		time.Sleep(250 * time.Millisecond) // TODO(tho) cfg
+
+	}
+}
+
+func updateExpVars(d *netem.NetemData) {
+	pktCount.Set(d.Total)
+	pktDropped.Set(d.Dropped)
+	pktReordered.Set(d.Reordered)
+	bytesCount.Set(d.Bytes)
+}
 
 func main() {
-	fmt.Println("TODO")
+	go NetemPoll()
+	http.ListenAndServe(":8080", nil)
 }
